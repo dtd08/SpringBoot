1d268a032b9bed9f2c94ba079c7903354e51a994
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-05-11 12:48:02 +0900

    Update MemberService.java

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,17 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
     // 회원 서비스 개발을 위한 회원 리포지토리 생성
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository; // Dependency Injection(DI) : 외부에서 리포지토리를 넣어줌
+    }
 
     // 회원 가입
     public Long join(Member member) {
@@ -34,12 +37,11 @@ public class MemberService {
     // 한 명만 조회
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
-    } 
+    }
 }
 
 // 리포지토리의 이름들은 단순 저장/조회에 가깝다면
-// 서비스는 좀 더 비즈니스적인 용어들을 씀
-// 그래야 소통하기 쉬움
+// 서비스는 좀 더 비즈니스적인 용어들을 씀 (소통을 위해)
 
 // 한마디로 서비스는 보통 비즈니스에 의존적으로 설계하고
 // 리포지토리의 경우는 좀 더 단순한, 기계적인? 용어를 쓴다.
