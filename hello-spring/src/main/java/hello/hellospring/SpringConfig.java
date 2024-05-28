8e9b1f19624c0f9ab4d33e2802241578e6aa5e6f
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-05-29 00:51:08 +0900

    Update SpringConfig.java

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @Configurable
 public class SpringConfig {
+
+    private DataSource dataSource; // 스프링이 알아서 설정 파일을 보고 자체적으로 빈을 생성해줌
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) { // 데이터 소스 주입
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource); // (config를 제외한) 다른 코드를 건들지 않고도 구현체를 통해 인터페이스를 확장함
     }
 }
