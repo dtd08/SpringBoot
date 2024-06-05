fed5fb0f05cf3bc0cf93026facf53c4606858c4b
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-06-05 23:47:59 +0900

    Update SpringConfig.java

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,30 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 
-import javax.sql.DataSource;
-
 @Configurable
 public class SpringConfig {
 
-    private DataSource dataSource; // 스프링이 알아서 설정 파일을 보고 자체적으로 빈을 생성해줌
+    /* private final DataSource dataSource; // 스프링이 알아서 설정 파일을 보고 자체적으로 빈을 생성해줌
 
     @Autowired
     public SpringConfig(DataSource dataSource) { // 데이터 소스 주입
         this.dataSource = dataSource;
+    } */
+
+    @PersistenceContext // 원래 스펙에서는 이렇게 받아야 함
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -28,6 +36,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource); // (config를 제외한) 다른 코드를 건들지 않고도 구현체를 통해 인터페이스를 확장함
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
