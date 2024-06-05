34056a43f4366263ebffe0ba7122ebb150e6667b
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-06-05 23:46:46 +0900

    Update Member.java

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,13 +1,20 @@
 package hello.hellospring.domain;
 
-public class Member { // 회원의 정보를 담은 구현체
+import jakarta.persistence.*;
+
+@Entity // jpa가 관리하는 entity라는 표현
+public class Member {
+
+    @Id // pk 등록
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에서 ID 값을 자동으로 생성함(= Identity 전략)을 알림
     private Long id; // 데이터 구분을 위해 시스템이 정하는 값
-    private String name; // 회원 이름
 
-
+//    @Column(name="username") // db의 컬럼명이 username으로 매핑됨  // 우리는 컬럼명과 변수명이 같으니 건들지 않아도 됨
     public String getName() {
         return name;
     }
+
+    private String name;
 
     public void setName(String name) {
         this.name = name;
