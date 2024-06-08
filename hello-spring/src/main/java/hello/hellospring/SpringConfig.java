5a0a658526a694061ac464d8c876078eec1b7c90
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-06-08 21:59:58 +0900

    Update SpringConfig.java

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,42 +1,53 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 
 @Configurable
 public class SpringConfig {
-
-    /* private final DataSource dataSource; // 스프링이 알아서 설정 파일을 보고 자체적으로 빈을 생성해줌
+    /*
+    private final DataSource dataSource; // 스프링이 알아서 설정 파일을 보고 자체적으로 빈을 생성해줌
 
     @Autowired
     public SpringConfig(DataSource dataSource) { // 데이터 소스 주입
         this.dataSource = dataSource;
     } */
 
-    @PersistenceContext // 원래 스펙에서는 이렇게 받아야 함
+    /* @PersistenceContext // 원래 스펙에서는 이렇게 받아야 함
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
+    } */
+
+    /* */
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+    @Bean // AOP 등록
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
+    }
+
+/*   @Bean
+     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource); // (config를 제외한) 다른 코드를 건들지 않고도 구현체를 통해 인터페이스를 확장함
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+     } */
 }
